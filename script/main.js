e673270b8988612a70acd526e890820f0eab9918
Ondřej Rejmont <rejmontondrej@gmail.com>
2021-10-04 17:20:43 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,39 @@
+// Menu-toggle button
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+    });
+
+    $("nav ul li").on("click", function() {
+        $("nav ul").toggleClass("showing");
+        $(".menu").toggleClass("close");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+        $('nav').addClass('.menu-scroll-style');
+    }
+
+    else {
+        $('nav').removeClass('black');
+        $('nav').removeClass('.menu-scroll-style');
+    }
+})
+
+// HAMBURGER    
+
+$("#wrapper").click( function() {
+	$(".menu").toggleClass("close");
+});
+
+
+
+window.addEventListener("load", function() {
+    var load_screen = document.getElementById("load_screen");
+    document.body.removeChild(load_screen);
+});
